9082ad3bbfba13092bf3a338c393772932247566
Ross Morgan-Linial <ross.morgan.linial@gmail.com>
2024-03-30 21:36:19 -0700

    LORA training fixes

--- dreambooth/diff_lora_to_sd_lora.py
+++ dreambooth/diff_lora_to_sd_lora.py
@@ -4,68 +4,13 @@ import os
 import safetensors.torch
 import torch
 
-
-unet_conversion_map_layer_reverse = {}
-
-for i in range(3):
-    # loop over downblocks/upblocks
-
-    for j in range(2):
-        # loop over resnets/attentions for downblocks
-        hf_down_res_prefix = f"down_blocks.{i}_{j}_"
-        sd_down_res_prefix = f"input_blocks_{3 * i + j + 1}_0_"
-        unet_conversion_map_layer_reverse[sd_down_res_prefix] = hf_down_res_prefix
-
-        if i > 0:
-            hf_down_atn_prefix = f"down_blocks.{i}_{j}_"
-            sd_down_atn_prefix = f"input_blocks_{3 * i + j + 1}_1_"
-            unet_conversion_map_layer_reverse[sd_down_atn_prefix] = hf_down_atn_prefix
-
-    for j in range(4):
-        # loop over resnets/attentions for upblocks
-        hf_up_res_prefix = f"up_blocks.{i}_{j}_"
-        sd_up_res_prefix = f"output_blocks_{3 * i + j}_0_"
-        unet_conversion_map_layer_reverse[sd_up_res_prefix] = hf_up_res_prefix
-
-        if i < 2:
-            # no attention layers in up_blocks.0
-            hf_up_atn_prefix = f"up_blocks.{i}_{j}_"
-            sd_up_atn_prefix = f"output_blocks_{3 * i + j}_1_"
-            unet_conversion_map_layer_reverse[sd_up_atn_prefix] = hf_up_atn_prefix
-
 # This is somewhat hacky, and I should probably feel bad...but I don't.
 to_replace = {
-    "text_encoder.text_model.encoder.": "lora_te1_text_model_encoder_",
-    "text_encoder_2.text_model.encoder.": "lora_te2_text_model_encoder_",
-    "to_q_": "to_q.",
-    "to_k_": "to_k.",
-    "to_v_": "to_v.",
-    "to_out_": "to_out_0.",
-    "lora.down": "lora_down",
-    "lora.up": "lora_up",
-    "self_attn.q": "self_attn_q",
-    "self_attn.k": "self_attn_k",
-    "self_attn.v": "self_attn_v",
-    "self_attn.out": "self_attn_out",
-    ".lora_linear_layer.down": ".lora_down",
-    ".lora_linear_layer.up": ".lora_up",
-    "mid_block_0": "middle_block_1",
-    "unet.": "lora_unet_",
+    ".": "_",
+    "_lora_up_": ".lora_up.",
+    "_lora_down_": ".lora_down.",
+    "unet_": "lora_unet_",
 }
-
-# This is excessive...but...
-for i in range(20):
-    to_replace[f".attentions.{i}.transformer"] = f"_{i}_transformer"
-
-for i in range(100):
-    to_replace[f"layers.{i}."] = f"layers_{i}_"
-
-# As is this
-for i in range(20):
-    for j in range(1,3):
-        to_replace[f"blocks.{i}.attn{j}.processor."] = f"blocks_{i}_attn{j}_"
-for (kk, vv) in unet_conversion_map_layer_reverse.items():
-    to_replace[vv] = kk
 
 # Check for missing alpha keys, set to 0.8 if not found
 base_attn_keys = [
@@ -95,10 +40,10 @@ def convert_diffusers_to_kohya_lora(path, metadata, alpha=0.8):
     alpha_keys = []
     # Replace the things
     for (key, v) in model_dict.items():
+        old_key = key
         for (kc,vc) in to_replace.items():
-            key = key.replace(kc, vc)
-        # We really shouldn't have to do this, but I don't want to change other things and break it, so...
-        key = key.replace("mid_block_0", "middle_block_1")
+            key = key.replace(kc, vc)            
+        print(f"convert_diffusers_to_kohya_lora: {old_key} -> {key}")
         akey = key
 
         # Check for missing alpha keys
